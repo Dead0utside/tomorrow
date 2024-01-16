9fd0683dfa5a41ebfa13fe564063dffb1757685d
Dead0utside <volodya1234321@gmail.com>
2024-01-16 19:46:41 +0100

    changed UserRepository from class to interface

--- src/main/java/com/dead0uts1de/tomorrow/user/UserRepository.java
+++ src/main/java/com/dead0uts1de/tomorrow/user/UserRepository.java
@@ -1,4 +1,6 @@
 package com.dead0uts1de.tomorrow.user;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
 }
