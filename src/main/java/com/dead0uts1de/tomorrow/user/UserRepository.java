cacb1acdc38d585774f0dd461fb60a8eafb82531
Dead0utside <volodya1234321@gmail.com>
2024-01-16 20:25:31 +0100

    connected database in application.properties; initialized UserService; created a query in UserRepository

--- src/main/java/com/dead0uts1de/tomorrow/user/UserRepository.java
+++ src/main/java/com/dead0uts1de/tomorrow/user/UserRepository.java
@@ -1,6 +1,13 @@
 package com.dead0uts1de.tomorrow.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    @Query("SELECT u FROM user WHERE u.id = ?1")
+    Optional<User> findUserById(Long id);
 }
---
+++ src/main/java/com/dead0uts1de/tomorrow/user/UserService.java
@@ -0,0 +1,8 @@
+package com.dead0uts1de.tomorrow.user;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+}
