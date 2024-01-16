8fc9cb05c1f844b9f42adcc9887d47a920d8738c
Dead0utside <volodya1234321@gmail.com>
2024-01-16 19:26:48 +0100

    does this work?

---
+++ src/main/java/com/dead0uts1de/tomorrow/task/Task.java
@@ -0,0 +1,12 @@
+package com.dead0uts1de.tomorrow.task;
+
+import java.util.Date;
+
+public class Task {
+    private Long id;
+    private String name;
+    private String description;
+    private Date creationDate;
+    private TaskType type;
+    private Date deadline;
+}
---
+++ src/main/java/com/dead0uts1de/tomorrow/task/TaskType.java
@@ -0,0 +1,4 @@
+package com.dead0uts1de.tomorrow.task;
+
+public enum TaskType {
+}
